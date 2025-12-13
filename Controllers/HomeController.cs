4510ff51dda3a76df4c9b669118ae793bfb11d5c
Manju <manju22573@gmail.com>
2025-12-13 14:49:49 -0500

    Added unit test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,38 @@ namespace MVCDeploy.Controllers
             return View();
         }
 
+        public IActionResult GetData()
+        {
+            var data = new
+            {
+                Message = "Hello from Controller",
+                Timestamp = DateTime.Now,
+                Items = new[] { "Item 1", "Item 2", "Item 3" },
+                Count = 3
+            };
+            
+            return Json(data);
+        }
+
+        public IActionResult GetWorcesterTemperature()
+        {
+            var random = new Random();
+            var temperature = random.Next(-5, 30);
+            
+            var weatherData = new
+            {
+                City = "Worcester",
+                State = "Massachusetts",
+                Country = "USA",
+                Temperature = temperature,
+                TemperatureUnit = "°C",
+                Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                Condition = temperature > 20 ? "Warm" : temperature > 10 ? "Mild" : "Cool"
+            };
+            
+            return Json(weatherData);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
